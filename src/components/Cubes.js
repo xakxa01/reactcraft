c7a005878f3c26ca9451e66a34e9eb1d2b3676d3
xakxa01 <Carlosxaviergomez@outlook.com>
2022-10-30 20:04:02 -0400

    transicion a arch

--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -1,14 +1,10 @@
-import { useStore } from "../hooks/useStore"
+import { useStore } from "../hooks/useStore";
 import Cube from "./Cube";
 
 export default function Cubes() {
-	const { cubes } = useStore();
+  const { cubes } = useStore();
 
-	return cubes.map(({ key, pos, texture }) => (
-		<Cube
-			key={key}
-			texture={texture}
-			position={pos}
-		/>
-	))
+  return cubes.map(({ key, pos, texture }) => (
+    <Cube key={key} texture={texture} position={pos} />
+  ));
 }
--- src/components/MenuInGame.js
+++ src/components/MenuInGame.js
@@ -2,36 +2,44 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function MenuInGame() {
+  const [show, setshow] = useState(false);
 
-	const [show, setshow] = useState(false)
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === "Escape") setshow(!show);
+    };
 
-	useEffect(() => {
-		const keyDownHandler = event => {
-			if (event.key === 'Escape') setshow(!show);
-		}
+    document.addEventListener("keydown", keyDownHandler);
 
-		document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [show]);
 
-		return () => {
-			document.removeEventListener('keydown', keyDownHandler);
-		};
+  if (show)
+    return (
+      <div
+        onClick={() => setshow(!show)}
+        className="absolute top-0 left-0 bg-gray-800 bg-opacity-75 w-screen h-screen flex flex-col justify-center items-center"
+      >
+        <button className="text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white">
+          guardar
+        </button>
 
-	}, [show]);
+        <button className="text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white">
+          resetear
+        </button>
 
-	if (show) return (
-		<div onClick={() => setshow(!show)} className="absolute top-0 left-0 bg-gray-800 bg-opacity-75 w-screen h-screen flex flex-col justify-center items-center">
-			<button className='text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white'>guardar</button>
+        <button clasasName="text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white">
+          controles
+        </button>
 
-			<button className='text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white'>resetear</button>
-
-			<button className='text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white'>controles</button>
-
-			<NavLink
-				to='/'
-				className='text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white'
-			>
-				volver
-			</NavLink>
-		</div>
-	)
+        <NavLink
+          to="/"
+          className="text-center uppercase py-5 px-10 w-80 bg-gray-400 text-4xl my-1 hover:bg-green-600 font-semibold text-gray-800 border-4 border-gray-500 hover:text-white"
+        >
+          volver
+        </NavLink>
+      </div>
+    );
 }
